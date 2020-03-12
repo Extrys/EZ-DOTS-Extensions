c971b3349d5c3b4a4f20949ef2b1c29ba67ccaeb
Extrys <extrysgo@gmail.com>
2020-03-12 17:03:22 +0100

    Update DOTSUtil.cs

--- DOTSUtil.cs
+++ DOTSUtil.cs
@@ -12,11 +12,13 @@ namespace SquirrelBytes.DOTS
 		}
 
 
-		/// <summary> This function is used inside the "for" conditional block, for repeating the interactions between entity components if possible </summary>
-		/// <param name="index">"for" index, used to defire the interaction direction</param><param name="getterA">ComponentDataFromEntity A</param><param name="getterB">ComponentDataFromEntity B</param>
+		/// <summary> This function is used inside the "for" conditional block,
+		/// for repeating the interactions between entity components if possible </summary>
+		/// <param name="index">"for" index, used to defire the interaction direction</param>
+		/// <param name="getterA">ComponentDataFromEntity A</param><param name="getterB">ComponentDataFromEntity B</param>
 		/// <param name="a">Entity A</param><param name="b">Entity B</param>
-		public static bool TryInteract<T, Y> (this EntityPair pair, 
-		      	int index, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB, out Entity a, out Entity b) 
+		public static bool TryInteract<T, Y> 
+			(this EntityPair pair,  int index, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB, out Entity a, out Entity b) 
 			where T : struct, IComponentData where Y : struct, IComponentData
 		{
 			bool switched = index > 0;
@@ -30,7 +32,8 @@ namespace SquirrelBytes.DOTS
 		/// <summary> This function is used to switch the pair and check the interaction compatibility between their components
 		/// can be used as condition for an iteration that ocurs two times for change the component compatibility check direction </summary>
 		///<param name = "getterA" > ComponentDataFromEntity A</param><param name = "getterB" > ComponentDataFromEntity B</param>
-		public static bool SwitchAndTryInteract<T, Y>(this ref EntityPair pair, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB) where T : struct, IComponentData where Y : struct, IComponentData
+		public static bool SwitchAndTryInteract<T, Y>
+			(this ref EntityPair pair, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB) where T : struct, IComponentData where Y : struct, IComponentData
 		{
 			pair.PairSwitch();
 			return getterA.Exists(pair.EntityA) & getterB.Exists(pair.EntityB);
