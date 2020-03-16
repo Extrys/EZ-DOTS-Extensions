126d644012f9d28fe9c9a7d5e3cceff85f0a22de
Extrys <extrysgo@gmail.com>
2020-03-16 16:19:38 +0100

    Update DOTSUtil.cs

--- DOTSUtil.cs
+++ DOTSUtil.cs
@@ -11,14 +11,14 @@ namespace SquirrelBytes.DOTS
 		}
 
 		public static bool TryInteract<T, Y>
-			(this EntityPair pair, int index, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB, out Entity a, out Entity b) 
+			(this EntityPair pair, ref int index, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB, out Entity a, out Entity b) 
 			where T : struct, IComponentData where Y : struct, IComponentData
 		{
 			bool switched = index > 0;
 			a = switched ? pair.EntityB : pair.EntityA;
 			b = switched ? pair.EntityA : pair.EntityB;
 
-			return index < 2 && getterA.Exists(a) & getterB.Exists(b);
+			return index++ < 2 && getterA.Exists(a) & getterB.Exists(b);
 		}
 
 		public static bool SwitchAndTryInteract<T, Y>(this ref EntityPair pair, ComponentDataFromEntity<T> getterA, ComponentDataFromEntity<Y> getterB) 
